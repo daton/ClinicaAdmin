873fde0cee67de54f3e737fe36d383bcf67596ec
Juan Carlos Campos <rapidclimate@gmail.com>
2018-06-26 23:36:30 -0500

    19-junio-2018

--- app/src/main/java/clinicamovil/unitec/org/clinicaadmin/MapsActivity.kt
+++ app/src/main/java/clinicamovil/unitec/org/clinicaadmin/MapsActivity.kt
@@ -1,7 +1,10 @@
 package clinicamovil.unitec.org.clinicaadmin
 
+import android.Manifest
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,10 +12,31 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import android.Manifest.permission
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager
+import android.support.v4.content.ContextCompat
+import android.support.annotation.NonNull
+import android.widget.Toast
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMyLocationClickListener,  ActivityCompat.OnRequestPermissionsResultCallback {
+
+
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
+
+    private var mPermissionDenied = false
 
     private lateinit var mMap: GoogleMap
+
+
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,14 +59,79 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+
+
         mMap.setMinZoomPreference(10.0f);
         mMap.setMaxZoomPreference(14.0f);
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(19.54, -99.0405251)
-        mMap.moveCamera((CameraUpdateFactory.zoomBy(13f)))
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+        enableMyLocation();
 
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(41.5043824,-87.2475618)
+       mMap.moveCamera((CameraUpdateFactory.zoomBy(13f)))
+      //  mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+       mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+    }
+
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    private fun enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true)
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.isMyLocationEnabled = true
+        }
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this, "Vamos a ubicarte!!", Toast.LENGTH_SHORT).show()
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false
+    }
+
+    override fun onMyLocationClick(location: Location) {
+        Toast.makeText(this, "Ubicación actual:\n$location", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                        Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation()
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true
+        }
+    }
+
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError()
+            mPermissionDenied = false
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private fun showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(supportFragmentManager, "dialog")
     }
 }
