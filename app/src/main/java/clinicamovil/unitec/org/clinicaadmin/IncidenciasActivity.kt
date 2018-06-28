b5b5e138b2cfc92703ffbec91346c94d003ce074
campitos <rapidclimate@gmail.com>
2018-06-27 23:09:11 -0500

    bbb

--- app/src/main/java/clinicamovil/unitec/org/clinicaadmin/IncidenciasActivity.kt
+++ app/src/main/java/clinicamovil/unitec/org/clinicaadmin/IncidenciasActivity.kt
@@ -1,31 +1,23 @@
 package clinicamovil.unitec.org.clinicaadmin
 
-import android.Manifest
+
 import android.annotation.SuppressLint
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.LocationManager
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
+
 import android.widget.Button
 import android.widget.Toast
-import android.content.Context.LOCATION_SERVICE
-import android.location.Location
-import android.location.LocationListener
 
-
-
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 
 class IncidenciasActivity : AppCompatActivity(),  ActivityCompat.OnRequestPermissionsResultCallback {
 
-    private val LOCATION_PERMISSION_REQUEST_CODE = 1
 
-    private var mPermissionDenied = false
-
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
 
     @SuppressLint("MissingPermission")
@@ -33,84 +25,28 @@ class IncidenciasActivity : AppCompatActivity(),  ActivityCompat.OnRequestPermis
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_incidencias)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         //Navegacion del incidencias
         val botonIncidencias=findViewById<Button>(R.id.obtenerPosicion)
         botonIncidencias.setOnClickListener{
 
+            fusedLocationClient.lastLocation
+                    .addOnSuccessListener { location : Location? ->
 
-            val locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
-// Define a listener that responds to location updates
-            val locationListener = object : LocationListener {
-                override fun onLocationChanged(location: Location) {
-                    // Called when a new location is found by the network location provider.
-                    //    makeUseOfNewLocation(location)
-                }
-
-                override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
-
-                override fun onProviderEnabled(provider: String) {}
-
-                override fun onProviderDisabled(provider: String) {}
-            }
-
-// Register the listener with the Location Manager to receive location updates
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
-
-            val locationProvider = LocationManager.GPS_PROVIDER
-// Or use LocationManager.GPS_PROVIDER
-
-            val lastKnownLocation = locationManager.getLastKnownLocation(locationProvider)
-
-              Toast.makeText(applicationContext,"Loca "+lastKnownLocation.latitude,Toast.LENGTH_LONG).show()
-        }
-
-
-
-
-        //Mio de googl android
-
-    }
-
-
-    private fun enableMyLocation() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // Permission to access the location is missing.
-            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
-                    Manifest.permission.ACCESS_FINE_LOCATION, true)
-
-
-            //Este es mio
-            // Acquire a reference to the system Location Manager
-
-
+                        Toast.makeText(applicationContext,"Loca "+location?.latitude+" Longi"+location?.longitude+" alti "+location?.altitude,Toast.LENGTH_LONG).show()
+                    }
 
         }
 
 
+
+
+
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
-                                            grantResults: IntArray) {
-        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
-            return
-        }
 
-        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
-                        Manifest.permission.ACCESS_FINE_LOCATION)) {
-            // Enable the my location layer if the permission has been granted.
-            enableMyLocation()
-        } else {
-            // Display the missing permission error dialog when the fragments resume.
-            mPermissionDenied = true
-        }
-    }
 
-    /**
-     * Displays a dialog with error message explaining that the location permission is missing.
-     */
-    private fun showMissingPermissionError() {
-        PermissionUtils.PermissionDeniedDialog
-                .newInstance(true).show(supportFragmentManager, "dialog")
-    }
+
+
 }
