b5b210705246d73df6dc28c6e4f89aad6b4bb33f
campitos <rapidclimate@gmail.com>
2018-06-27 20:01:01 -0500

    bbb

--- app/src/main/java/clinicamovil/unitec/org/clinicaadmin/IncidenciasActivity.kt
+++ app/src/main/java/clinicamovil/unitec/org/clinicaadmin/IncidenciasActivity.kt
@@ -1,13 +1,34 @@
 package clinicamovil.unitec.org.clinicaadmin
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.widget.Button
 import android.widget.Toast
+import android.content.Context.LOCATION_SERVICE
+import android.location.Location
+import android.location.LocationListener
 
-class IncidenciasActivity : AppCompatActivity()  {
 
+
+
+
+class IncidenciasActivity : AppCompatActivity(),  ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
+
+    private var mPermissionDenied = false
+
+
+
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_incidencias)
@@ -15,7 +36,81 @@ class IncidenciasActivity : AppCompatActivity()  {
         //Navegacion del incidencias
         val botonIncidencias=findViewById<Button>(R.id.obtenerPosicion)
         botonIncidencias.setOnClickListener{
-              Toast.makeText(applicationContext,"Mensajito",Toast.LENGTH_LONG).show()
+
+
+            val locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+// Define a listener that responds to location updates
+            val locationListener = object : LocationListener {
+                override fun onLocationChanged(location: Location) {
+                    // Called when a new location is found by the network location provider.
+                    //    makeUseOfNewLocation(location)
+                }
+
+                override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+
+                override fun onProviderEnabled(provider: String) {}
+
+                override fun onProviderDisabled(provider: String) {}
+            }
+
+// Register the listener with the Location Manager to receive location updates
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationListener)
+
+            val locationProvider = LocationManager.GPS_PROVIDER
+// Or use LocationManager.GPS_PROVIDER
+
+            val lastKnownLocation = locationManager.getLastKnownLocation(locationProvider)
+
+              Toast.makeText(applicationContext,"Loca "+lastKnownLocation.latitude,Toast.LENGTH_LONG).show()
         }
+
+
+
+
+        //Mio de googl android
+
+    }
+
+
+    private fun enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true)
+
+
+            //Este es mio
+            // Acquire a reference to the system Location Manager
+
+
+
+        }
+
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                        Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation()
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private fun showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(supportFragmentManager, "dialog")
     }
 }
