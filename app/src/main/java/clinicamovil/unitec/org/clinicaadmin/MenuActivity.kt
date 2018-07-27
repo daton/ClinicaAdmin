1f5a007da263f1bf31b5f254792a70aad13a323c
Juan Carlos Campos <rapidclimate@gmail.com>
2018-07-27 12:47:41 -0500

    21-julio-2018

--- app/src/main/java/clinicamovil/unitec/org/clinicaadmin/MenuActivity.kt
+++ app/src/main/java/clinicamovil/unitec/org/clinicaadmin/MenuActivity.kt
@@ -1,6 +1,7 @@
 package clinicamovil.unitec.org.clinicaadmin
 
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
@@ -8,8 +9,11 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_menu.*
 import kotlinx.android.synthetic.main.app_bar_menu.*
+
 
 class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -29,6 +33,51 @@ class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+
+
+
+
+
+
+        //Mostramos solo el contenido0 principalk
+        ocultarTodo()
+        val principal = findViewById(R.id.principal) as ConstraintLayout
+        principal.visibility = View.VISIBLE
+
+
+
+
+        //
+        //Ajustamos el spinner
+
+        var spinner=   findViewById<Spinner>(R.id.spinnerIncidencias)
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        val adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item)
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+// Apply the adapter to the spinner
+        spinner.adapter = adapter
+        spinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                var item=        parent?.getItemAtPosition(position)
+                //El siguiente funciona bien
+                //Toast.makeText(applicationContext,"Este es "+item,Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        // Guardamos la incidencia
+           findViewById<Button>(R.id.guardarIncidencia).setOnClickListener{
+            var item=   spinner?.selectedItem
+               Toast.makeText(applicationContext,"Seleccionaste "+item,Toast.LENGTH_SHORT).show();
+           }
+
     }
 
     override fun onBackPressed() {
@@ -58,27 +107,36 @@ class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
+            R.id.principal -> {
                 // Handle the camera action
-            }
-            R.id.nav_gallery -> {
+                ocultarTodo()
+                val principal = findViewById(R.id.principal) as ConstraintLayout
+                principal.visibility = View.VISIBLE
 
             }
-            R.id.nav_slideshow -> {
+            R.id.incidencias -> {
+                ocultarTodo()
+                val incidencias = findViewById(R.id.incidencias) as ConstraintLayout
+                incidencias.visibility = View.VISIBLE
+
+
+
 
             }
-            R.id.nav_manage -> {
+            R.id.clinicas -> {
 
             }
-            R.id.nav_share -> {
 
-            }
-            R.id.nav_send -> {
-
-            }
         }
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    fun ocultarTodo(){
+        val principal = findViewById(R.id.principal) as ConstraintLayout
+        val incidencias = findViewById(R.id.incidencias) as ConstraintLayout
+        principal.visibility = View.INVISIBLE
+        incidencias.visibility = View.INVISIBLE
     }
 }
