be87c21e892c5e8a0624a3e93ead88a2cad70179
Juan Carlos Campos <rapidclimate@gmail.com>
2018-07-31 13:58:22 -0500

    Login con spinner OK

--- app/src/main/java/clinicamovil/unitec/org/clinicaadmin/LoginActivity.kt
+++ app/src/main/java/clinicamovil/unitec/org/clinicaadmin/LoginActivity.kt
@@ -4,16 +4,17 @@ import android.content.Intent
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
 
 class LoginActivity : AppCompatActivity() {
 
-    var academico=Academico()
-    var estatus=Estatus()
+    var academico: Academico? = null
+    var estatus = Estatus()
+    var nip: Int? = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,47 +22,81 @@ class LoginActivity : AppCompatActivity() {
 
         getSupportActionBar()?.hide();
 
-         findViewById<Button>(R.id.botonRegistrarse).setOnClickListener {
-             var i= Intent(application, MainActivity::class.java);
-             startActivity(i);
-         }
+        findViewById<Button>(R.id.botonRegistrarse).setOnClickListener {
+            var i = Intent(application, MainActivity::class.java);
+            startActivity(i);
+        }
 
 
         findViewById<Button>(R.id.botonIngresar).setOnClickListener {
-            var i=Intent(applicationContext, MenuActivity::class.java)
 
-            startActivity(i)
-            TareaAutenticar().execute(null,null,null)
+            var progreso = findViewById<ProgressBar>(R.id.progressBar1)
+            progreso.visibility = View.VISIBLE
+            TareaAutenticar().execute(null, null, null)
         }
     }
-     inner class TareaAutenticar:AsyncTask<Void,Void,Void>(){
-         override fun onPostExecute(result: Void?) {
-             super.onPostExecute(result)
-            Toast.makeText(applicationContext,"El rol es "+academico.rol,Toast.LENGTH_LONG).show()
-         }
 
-         override fun doInBackground(vararg params: Void?): Void ?{
-             var url2="https://node74674-env-8686050.whelastic.net/api/academico/"+33868
+    inner class TareaAutenticar : AsyncTask<Void, Void, Void>() {
+        override fun onPostExecute(result: Void?) {
+            super.onPostExecute(result)
+            nip = -1
+            if (academico == null) {
+                Toast.makeText(applicationContext, "NO te autenticaste", Toast.LENGTH_LONG).show()
+                var txtError = findViewById<TextView>(R.id.txtErrorNip)
+                txtError.visibility = View.VISIBLE
+                txtError.text = "NIP incorrecto"
+            } else {
+                var txtError = findViewById<TextView>(R.id.txtErrorNip)
+                txtError.visibility = View.INVISIBLE
+                Toast.makeText(applicationContext, "El rol es " + academico?.rol, Toast.LENGTH_LONG).show()
 
-             val restTemplate = RestTemplate()
-             restTemplate.messageConverters.add(MappingJackson2HttpMessageConverter())
+                var i = Intent(applicationContext, MenuActivity::class.java)
+
+                startActivity(i)
+            }
+            var progreso = findViewById<ProgressBar>(R.id.progressBar1)
+            progreso.visibility = View.GONE
+
+        }
+
+        override fun doInBackground(vararg params: Void?): Void? {
+            var url2 = "https://node74674-env-8686050.whelastic.net/api/academico/" + nip
+            //  var url2="http://192.168.100.12/api/academico/"+33868
+
+            val restTemplate = RestTemplate()
+            restTemplate.messageConverters.add(MappingJackson2HttpMessageConverter())
 
 
-             val maper = ObjectMapper()
-             //  usuarios = maper.readValue(estring, object : TypeReference<ArrayList<Usuario>>() {})
+            val maper = ObjectMapper()
+            //  usuarios = maper.readValue(estring, object : TypeReference<ArrayList<Usuario>>() {})
 
-             val respuesta = restTemplate.getForObject(url2,  String::class.java)
-             academico = maper.readValue(respuesta,Academico::class.java )
-             print("El rol es"+academico.rol)
+            val respuesta = restTemplate.getForObject(url2, String::class.java)
+            if (respuesta != null)
+                academico = maper.readValue(respuesta, Academico::class.java)
+            else academico = null
+            print("El rol es" + academico?.rol)
 
-             println("DESPUES DE REST");
-             return null
+            println("DESPUES DE REST");
+            return null
 
-         }
+        }
 
 
-         override fun onPreExecute() {
-             super.onPreExecute()
-         }
-     }
+        override fun onPreExecute() {
+            super.onPreExecute()
+
+            var txtError = findViewById<TextView>(R.id.txtErrorNip)
+            txtError.visibility = View.INVISIBLE
+
+            try {
+                nip = findViewById<EditText>(R.id.textonip).text.toString().toInt();
+
+                Toast.makeText(applicationContext, "UN numero " + nip, Toast.LENGTH_LONG).show()
+            } catch (e: NumberFormatException) {
+                var txtError = findViewById<TextView>(R.id.txtErrorNip)
+                txtError.visibility = View.VISIBLE
+                txtError.text = "NIP incorrecto"
+            }
+        }
+    }
 }
